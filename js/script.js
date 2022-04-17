26170bf25e1f07eef901a3def4635d5e65c2cef2
ameer <ameer.em.89@gmail.com>
2022-04-17 17:15:55 +0300

    Add this homework with localstorage code

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var sum = 0;
+var arr;
+if(localStorage.getItem('storedata') != null)
+{
+  arr=JSON.parse(localStorage.getItem('storedata'));
+ 
+}
+// IN IF (FOR OLD DATA)
+
+else 
+{
+  arr=[];
+}
+
+function stam_func() {
+  let elemnts = [];
+  elemnts[0] = document.getElementById("student");
+  elemnts[1] = document.getElementById("score");
+  elemnts[2] = document.getElementById("mytable");
+  if (elemnts[0].value == "") {
+    alert("please enter a student name");
+  } else if (elemnts[1].value == "") {
+    alert("please enter a student score");
+  } else if (isNaN(elemnts[1].value)) {
+    alert("please enter a number for score");
+  } else {
+    let ele1 = document.createElement("label");
+    let ele2 = document.createElement("label");
+    let ele3 = document.createElement("td");
+    let ele4 = document.createElement("td");
+    let ele5 = document.createElement("tr");
+    ele1.textContent = elemnts[0].value;
+    ele2.textContent = elemnts[1].value;
+    ele3.appendChild(ele1);
+    ele4.appendChild(ele2);
+    ele5.appendChild(ele3);
+    ele5.appendChild(ele4);
+    elemnts[2].appendChild(ele5);
+
+    sum += Number(elemnts[1].value);
+    var lines = elemnts[2].getElementsByTagName("tr");
+    let x1 = document.getElementById("num");
+    x1.textContent = lines.length;
+    let x2 = document.getElementById("avg");
+    x2.textContent = sum / lines.length;
+
+    arr.push(y);
+    localStorage.setItem('storedata', JSON.stringify(arr));
+    console.log(arr);
+  }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+
+# This homework contains 2 inputs, table and two cards:
+1. input for students names
+2. input for students scores
+3. table contains students names and students scores 
+4. The first card contains students scores average
+5. The second card contains the number of students names
+
+
+
+## In this homework I wrote a JavaScript code, Html page, CSS page.
+
+# Installing
+1. Copy my https repository
+2. choose any terminal
+3. Git Clone- work locally with your computer
+4. Copy Repo URL
+5. cd (The name of the repository)- choose the file homework which you want.
+6. type a command code .
+
+# All Rights Reserved- By Ameer Emran: Fullstack student developer: 13.04.2022
